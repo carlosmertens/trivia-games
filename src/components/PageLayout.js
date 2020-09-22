e38df6ceb0832dd5cc75229468a91000107e490a
Carlos Mertens <mertenscarlos@yahoo.com>
2020-09-22 12:17:41 +0200

    Create page layout

---
+++ src/components/PageLayout.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PageLayout = ({ children }) => {
+  return <div className='container-fluid text-center'>{children}</div>;
+};
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom';
 import '../styles/App.css';
 
 import { PageHeader } from '../components/PageHeader';
+import { PageLayout } from '../components/PageLayout';
 
 function Home() {
   return (
-    <div className='container text-center'>
+    <PageLayout>
       <PageHeader>Welcome to the Trivia Challange!</PageHeader>
       <div className='container'>
         <p>You will be presented with 10 True or False questions.</p>
         <p>Can you score 100%?</p>
       </div>
       <Link to='/quizes' className='btn btn-outline-primary btn-lg'>
-        Play Now
+        Begin
       </Link>
-    </div>
+    </PageLayout>
   );
 }
 
